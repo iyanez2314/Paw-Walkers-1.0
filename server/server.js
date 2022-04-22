69fad7f6253f3f2cc35faf1a236ca586b9b98ef4
Isaac Yanez <isaacyanez@Isaacs-MBP-2.lan>
2022-04-22 16:25:39 -0500

    added a line of code in server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const db = require('./config/connection');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,3 +14,8 @@ app.use(require('./Routes'));
 mongoose.set('debug', true);
 
 
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
